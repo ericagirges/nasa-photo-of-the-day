2da9d35a134fdf70d16c0a6371f4c660228b002a
Erica Girges <ericafiorenza@gmail.com>
2020-08-12 14:30:53 -0700

    App.js completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./App.css";
+import Header from "./Header";
+import Date from "./Date";
+import PhotoOfDay from "./PhotoOfDay";
+import {BASE_URL, API_KEY} from "./index"
+// import axios
+import axios from "axios";
+
 
 function App() {
+  const [photoOfDay, setPhotoOfDay] = useState(null);
+  const [date, setDate] = useState(null);
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}?api_key=${API_KEY}`)
+    .then(response => {
+      console.log(response)
+      setPhotoOfDay(response.data)
+    } )
+    .catch(error => {
+      debugger
+    })
+  }, [date])
   return (
     <div className="App">
-      <p>
+      {/* <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      </p> */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+export const BASE_URL = 'https://api.nasa.gov/planetary/apod'
+export const API_KEY = 'BU3D0Wl4R4zn4LUEMdIBvzUjhW0S0a1E5C46IwHu'
 
 ReactDOM.render(<App />, document.getElementById("root"));
